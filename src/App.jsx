746608f746d7d009ad27a0581d41f7a948264433
Kyara <kyara0324@naver.com>
2024-05-27 20:55:08 +0900

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  const plusCount = () => {
-    setCount(count + 1);
-  };
-
-  const minusCount = () => {
-    setCount(count - 1);
-  };
-
-  const resetCount = () => {
-    setCount(0);
-  };
-
-  return (
-    <>
-      <div className="container">
-        <div className="button-group">
-          <button onClick={minusCount}>-</button>
-          Count: {count}
-          <button onClick={plusCount}>+</button>
-        </div>
-        <button onClick={resetCount}>reset</button>
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
