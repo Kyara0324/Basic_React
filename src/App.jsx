83b6addfc5aa338c8de55163517540d9c15a2ff2
Kyara <kyara0324@naver.com>
2024-05-30 10:56:12 +0900

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { Fragment, useState } from "react";
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  const plusCount = () => {
+    setCount(count + 1);
+  };
+
+  const minusCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="button-group">
+          <button onClick={minusCount}>-</button>
+          Count: {count}
+          <button onClick={plusCount}>+</button>
+        </div>
+        <button onClick={resetCount}>reset</button>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
